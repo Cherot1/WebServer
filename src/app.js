e7ff29488d229a6465ad32008bb61f488bc57d3f
Cherot <hectornovoadita2000@gmail.com>
2022-09-15 05:30:54 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ udp.bind(udpPort,udpHost);
 
 app.get("/data", (req,res) =>{
     dbdata = cnx.getgpsdata();
-    console.log(dbdata.fecha);
+    console.log(dbdata);
     res.json({
         "lat": data[0].substr(0,8),
         "lon": data[1].substr(0,9),
