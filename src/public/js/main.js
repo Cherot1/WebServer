05eddae4811619173ac6166434878bf963fbf0ce
Cherot <hectornovoadita2000@gmail.com>
2022-09-15 11:13:37 -0500

    nn

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,13 +1,4 @@
 // build leaflet map with an spific template
-<<<<<<< HEAD
-const map = L.map('map-template').setView([11.022, -74.869], 13);
-const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-L.tileLayer(tileURL).addTo(map);
-
-marker = L.marker([11.022, -74.869],13)
-marker.bindPopup('11.022 | -74.869');
-map.addLayer(marker);
-=======
 
 const map = L.map('map-template', {zoomControl: false}).setView([10.965633, -74.8215339], 12.5);const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 L.tileLayer(tileURL).addTo(map);
@@ -27,7 +18,6 @@ let prelat = 0;
 let prelon = 0;
 
 
->>>>>>> d7121e11c5cd28d5a1ea7e41503b16a0611cbc3f
 
 async function getData(){
     const response = await fetch("./data", {});
@@ -37,16 +27,6 @@ async function getData(){
     document.getElementById("time").innerHTML = await `${responseJson.tm}`;
     document.getElementById("lat").innerHTML = await `${responseJson.lat}`;
     document.getElementById("lon").innerHTML = await `${responseJson.lon}`;
-<<<<<<< HEAD
-
-    if(responseJson.lat != 0){
-        map.setView([parseFloat(responseJson.lat), parseFloat(responseJson.lon)], 13);
-        map.removeLayer(marker);
-        marker = new L.marker([parseFloat(responseJson.lat), parseFloat(responseJson.lon)]);
-        marker.bindPopup(responseJson.lat+" | "+responseJson.lon);
-        map.addLayer(marker);
-    }
-=======
     lat = parseFloat(responseJson.lat);
     lon = parseFloat(responseJson.lon);
 
@@ -64,7 +44,6 @@ async function getData(){
     }
     prelat = lat;
     prelon = lon;
->>>>>>> d7121e11c5cd28d5a1ea7e41503b16a0611cbc3f
     
 }
 
