849ba1a505ee278ad5c444d0e415bcb44af2278e
Cherot <hectornovoadita2000@gmail.com>
2022-09-15 09:04:47 -0500

    backend db

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ udp.bind(udpPort,udpHost);
 
 app.get("/data", (req,res) =>{
     let dbdata = cnx.getgpsdata();
-    console.log(dbdata);
+    console.log(dbdata[0].fecha);
     res.json({
         "lat": data[0].substr(0,8),
         "lon": data[1].substr(0,9),
