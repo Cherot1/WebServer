8a7c303d028ab585148ce01c4e487a8b132cc9a5
isapacheco08 <ispafo@gmail.com>
2022-09-28 11:17:38 -0500

    idk16

--- src/app.js
+++ src/app.js
@@ -81,7 +81,7 @@ app.listen(port, () => {
 
 app.post("/place", (req,res) =>{
 
-    let querym= "SELECT fecha, hora FROM gps_data WHERE latitud BETWEEN ("+req.body.latp+"*0.99997) AND  ("+req.body.latp+"*1.00005) AND longitud BETWEEN ("+req.body.lonp+"*1.00005) AND  ("+req.body.lonp+"*0.99997)";
+    let querym= "SELECT fecha, hora FROM gps_data WHERE latitud BETWEEN (' "+req.body.latp+"*0.99997 ' ) AND  (' "+req.body.latp+"*1.00005 ') AND longitud BETWEEN (' "+req.body.lonp+"*1.00005 ') AND  (' "+req.body.lonp+"*0.99997 ')";
     cnx.pool.query(querym, (err,rows) => {
         if (err) throw err;
         res.json({
@@ -89,7 +89,7 @@ app.post("/place", (req,res) =>{
         })
     });
         console.log(data);
-});
+}); 
 
 
 
