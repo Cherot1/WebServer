9d98db04942bd8ff4448cc1b97df023b88d7f1eb
isapacheco08 <ispafo@gmail.com>
2022-09-28 20:08:56 -0500

    idk60

--- src/database/db.sql
+++ src/database/db.sql
@@ -9,8 +9,8 @@ CREATE TABLE gps_data(
          id INT(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
          fecha DATE NOT NULL,
          hora TIME NOT NULL,
-         latitud VARCHAR(13) NOT NULL,
-         longitud VARCHAR(13) NOT NULL
+         latitud FLOAT (13) NOT NULL,
+         longitud FLOAT (13) NOT NULL
 );
 
 -- To show all tables
