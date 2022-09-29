40dd6d7da986a3f9dddd705092641ab459765a33
jdestrada12 <jdestrada@uninorte.edu.co>
2022-09-28 19:40:09 -0500

    m278'

--- src/app.js
+++ src/app.js
@@ -66,14 +66,16 @@ app.post("/place", (req,res) =>{
 
    
 
+
     let querym= "SELECT DISTINCT fecha, hora FROM Datosgps.gps_data WHERE latitud BETWEEN ('"+req.body.latp+"'*0.99997) and ('"+req.body.latp+"'*1.00005) and longitud BETWEEN ('"+req.body.longp+"'*1.00005) and ('"+req.body.longp+"'*0.99997)  "
+    console.log(querym);
     cnx.pool.query(querym, (err,rows) => {
         if (err) throw err;
         res.json({
             datap : rows
         })
     });
-    console.log(datap);
+    
 
 }); 
 
